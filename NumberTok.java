459182625501609da0705f15f96010ac162b45b6
Pit-Red <118454728+Pit-Red@users.noreply.github.com>
2023-01-26 13:19:41 +0100

    Delete NumberTok.java

--- NumberTok.java
+++
@@ -1,9 +0,0 @@
-public class NumberTok extends Token {
-	public static int valore;
-	public String toString() { return "<" + tag + ", " + valore + ">"; }
-
-	public NumberTok(int tag, String s){
-		super(tag);
-		valore = Integer.parseInt(s);
-	}
-}
